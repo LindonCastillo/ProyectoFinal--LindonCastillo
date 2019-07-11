1e1722fe7781efadc7ee978896393ad3894492b4
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-07-11 18:04:10 -0400

    Consulta terminada

--- ProyectoCooasar/ProyectoCooasar/UI/Consultas/cUsuarios.Designer.cs
+++ ProyectoCooasar/ProyectoCooasar/UI/Consultas/cUsuarios.Designer.cs
@@ -31,15 +31,15 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cUsuarios));
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Criterio_textBox = new System.Windows.Forms.TextBox();
             this.Filtro_comboBox = new System.Windows.Forms.ComboBox();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
+            this.Consulta_dataGridView = new System.Windows.Forms.DataGridView();
+            this.Consultar_button = new System.Windows.Forms.Button();
             this.Hasta_dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.Desde_dateTimePicker = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Consulta_dataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label3
@@ -62,13 +62,13 @@
             this.label2.TabIndex = 11;
             this.label2.Text = "Filtro";
             // 
-            // textBox1
+            // Criterio_textBox
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(376, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(185, 22);
-            this.textBox1.TabIndex = 10;
+            this.Criterio_textBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Criterio_textBox.Location = new System.Drawing.Point(376, 28);
+            this.Criterio_textBox.Name = "Criterio_textBox";
+            this.Criterio_textBox.Size = new System.Drawing.Size(185, 22);
+            this.Criterio_textBox.TabIndex = 10;
             // 
             // Filtro_comboBox
             // 
@@ -84,29 +84,30 @@
             this.Filtro_comboBox.Size = new System.Drawing.Size(118, 23);
             this.Filtro_comboBox.TabIndex = 9;
             // 
-            // dataGridView1
+            // Consulta_dataGridView
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 57);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(657, 322);
-            this.dataGridView1.TabIndex = 7;
+            this.Consulta_dataGridView.AllowUserToAddRows = false;
+            this.Consulta_dataGridView.AllowUserToDeleteRows = false;
+            this.Consulta_dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Consulta_dataGridView.Location = new System.Drawing.Point(12, 57);
+            this.Consulta_dataGridView.Name = "Consulta_dataGridView";
+            this.Consulta_dataGridView.ReadOnly = true;
+            this.Consulta_dataGridView.Size = new System.Drawing.Size(657, 322);
+            this.Consulta_dataGridView.TabIndex = 7;
             // 
-            // button1
+            // Consultar_button
             // 
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Image = global::ProyectoCooasar.Properties.Resources.seo_social_web_network_internet_340_icon_icons_com_61497;
-            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.button1.Location = new System.Drawing.Point(567, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(102, 27);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Consultar";
-            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.button1.UseVisualStyleBackColor = true;
+            this.Consultar_button.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Consultar_button.Image = global::ProyectoCooasar.Properties.Resources.seo_social_web_network_internet_340_icon_icons_com_61497;
+            this.Consultar_button.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Consultar_button.Location = new System.Drawing.Point(567, 23);
+            this.Consultar_button.Name = "Consultar_button";
+            this.Consultar_button.Size = new System.Drawing.Size(102, 27);
+            this.Consultar_button.TabIndex = 8;
+            this.Consultar_button.Text = "Consultar";
+            this.Consultar_button.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Consultar_button.UseVisualStyleBackColor = true;
+            this.Consultar_button.Click += new System.EventHandler(this.Consultar_button_Click);
             // 
             // Hasta_dateTimePicker
             // 
@@ -159,14 +160,14 @@
             this.Controls.Add(this.Hasta_dateTimePicker);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Criterio_textBox);
             this.Controls.Add(this.Filtro_comboBox);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Consultar_button);
+            this.Controls.Add(this.Consulta_dataGridView);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "cUsuarios";
             this.Text = "Consulta de Usuarios";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Consulta_dataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -176,10 +177,10 @@
 
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox Criterio_textBox;
         private System.Windows.Forms.ComboBox Filtro_comboBox;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button Consultar_button;
+        private System.Windows.Forms.DataGridView Consulta_dataGridView;
         private System.Windows.Forms.DateTimePicker Hasta_dateTimePicker;
         private System.Windows.Forms.DateTimePicker Desde_dateTimePicker;
         private System.Windows.Forms.Label label1;
--- ProyectoCooasar/ProyectoCooasar/UI/Consultas/cUsuarios.cs
+++ ProyectoCooasar/ProyectoCooasar/UI/Consultas/cUsuarios.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProyectoCooasar.BLL;
+using ProyectoCooasar.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +17,51 @@ namespace ProyectoCooasar.UI.Consultas
         public cUsuarios()
         {
             InitializeComponent();
+        }
+
+        private void Consultar_button_Click(object sender, EventArgs e)
+        {
+            RepositorioBase<Usuarios> repositorio = new RepositorioBase<Usuarios>();
+            var listado = new List<Usuarios>();
+            if (Criterio_textBox.Text.Trim().Length > 0)
+            {
+                switch (Filtro_comboBox.SelectedIndex)
+                {
+                    case 0://Todo
+                        listado = repositorio.GetList(p => true);
+                        break;
+
+                    case 1://Id
+                        int id = 0;
+                        if (int.TryParse(Criterio_textBox.Text, out id))
+                        {
+                            id = Convert.ToInt32(Criterio_textBox.Text);
+                            listado = repositorio.GetList(p => p.UsuarioId == id);
+                        }
+                        else
+                        {
+                            MessageBox.Show("No Puedes Hacer Esto");
+                        }
+                        break;
+
+                    case 2://Nombre
+                        listado = repositorio.GetList(p => p.Nombre.Contains(Criterio_textBox.Text));
+                        break;
+
+                    case 3://Usuario
+                        listado = repositorio.GetList(p => p.Usuario.Contains(Criterio_textBox.Text));
+                        break;
+                }
+
+                listado = listado.Where(c => c.FechaIngreso.Date >= Desde_dateTimePicker.Value.Date && c.FechaIngreso.Date <= Hasta_dateTimePicker.Value.Date).ToList();
+            }
+            else
+            {
+                listado = repositorio.GetList(p => true);
+            }
+
+            Consulta_dataGridView.DataSource = null;
+            Consulta_dataGridView.DataSource = listado;
         }
     }
 }
