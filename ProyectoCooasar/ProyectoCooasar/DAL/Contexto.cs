e7eb7fd97df44ef8af08e24adef0941cf5b3a2b3
Lindon Emil Castillo Burgos <lindon_castillo@ucne.edu.do>
2019-07-11 16:51:45 -0400

    Agregando el IRepository y el RepositorioBase

---
+++ ProyectoCooasar/ProyectoCooasar/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using ProyectoCooasar.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCooasar.DAL
+{
+        public class Contexto : DbContext
+        {
+            public DbSet<Usuarios> Usuario { get; set; }
+            public Contexto() : base("ConStr")
+            { }
+        }
+}
---
+++ ProyectoCooasar/ProyectoCooasar/Entidades/Productos.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoCooasar.Entidades
+{
+    public class Productos
+    {
+        [Key]
+        public int ProductoId { get; set; }
+        public string Nombre { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioCompra { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public int ITBIS { get; set; }
+        public int UsuarioId { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Nombre = string.Empty;
+            Fecha = DateTime.Now;
+            Cantidad = 0;
+            PrecioCompra = 0;
+            PrecioVenta = 0;
+            ITBIS = 0;
+            UsuarioId = 0;
+        }
+    }
+}
